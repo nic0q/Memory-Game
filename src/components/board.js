18134009c9e5156bd96b2b069c504938e0893da6
unknown <nicolas.farfan.ch@usach.cl>
2022-03-01 01:24:37 -0300

    veo la luz al final del tunel, la aplicacion funciona (beta)

--- src/components/board.js
+++ src/components/board.js
@@ -1,22 +1,62 @@
-import Grid from "./grid"
 import emojis from "../services/tokens/emojis"
-import characters from "../services/tokens/characters" // Characters for use in memory game
+// import characters from "../services/tokens/characters" // Characters for use in memory game
 import shuffle from "../utilities/shuffleList"
 import "../styles/board.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import "../styles/grid.css"
 
+const grid_positions = shuffle([
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+])
+
+const Grid = ({ classNames, content }) => {
+  return <div className={classNames}>{content}</div>
+}
+const arr = Array(16).fill(0)
 export default function Tablero() {
-  const [actives, isActive] = useState(0)
-  const grid_positions = shuffle([
-    0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7,
-  ])
-  let a = -1
+  const [fliped, setFliped] = useState([])
+  const [reset, setReset] = useState(false)
+
   console.log(grid_positions)
+
+  const play = (card) => {
+    setReset(false)
+    setFliped(() => [...fliped, card])
+    arr[card] = 1
+  }
+  if (fliped.length === 2) {
+    if (emojis()[fliped[0]] === emojis()[fliped[1]]) {
+      console.log("correct")
+    } else {
+      console.log("inco")
+      arr[fliped[0]] = 0
+      arr[fliped[1]] = 0
+      arr[fliped[2]] = 0
+    }
+    setFliped([])
+    setReset(true)
+  }
+  console.log(fliped)
   return (
     <div className="board">
-      {grid_positions.map((e) => {
-        a += 1
-        return <Grid key={a} styles={emojis()} number={e}></Grid>
+      {grid_positions.map((n) => {
+        return arr[n] === 1 ? (
+          <div key={n} onClick={() => play(n)}>
+            <Grid
+              content={emojis()[n]}
+              // reset={reset}
+              classNames={"grid animationBack"}
+            ></Grid>
+          </div>
+        ) : (
+          <div key={n} onClick={() => play(n)}>
+            <Grid
+              content={""}
+              // reset={reset}
+              classNames={"grid"}
+            ></Grid>
+          </div>
+        )
       })}
     </div>
   )
--- src/components/grid.js
+++ src/components/grid.js
@@ -1,23 +1,6 @@
 import "../styles/grid.css"
 import { useState } from "react"
-const arr = new Array(16).fill(0)
+const arr = new Array(8).fill(0)
+const play = new Array(1)
+let actives = 1
 
-export default function Grid({ styles, number }) {
-  const [isFliped, setIsFliped] = useState(false)
-  
-  const handleClick = () => {
-    setIsFliped(true)
-    arr[number] = 1
-    console.log(arr)
-    console.log(isFliped)
-  }
-  return !isFliped ? ( // if not isFliped
-    <div>
-      <div className="grid" onClick={handleClick}></div>
-    </div>
-  ) : (
-    <div>
-      <div className="grid animationBack">{styles[number]}</div>
-    </div>
-  )
-}
